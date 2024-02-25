f4e5ccd8fdb1c51c020a210fdd6423aa9f63bf17
roman.richter <richter@365farmnet.com>
2024-02-25 13:56:09 +0100

    Added new Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,22 @@
-import logo from '../../assets/logo.png';
-import './Header.css';
+import reactImg from '../../assets/react-core-concepts.png'
+import './Header.css'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
 
 export default function Header() {
+  const description = reactDescriptions[genRandomInt(2)]
   return (
-    <header className="flex flex-col items-center mt-8 md:mb-16">
-      <img
-        src={logo}
-        alt="A canvas"
-        className={'mb-8 w-44 h-44 object-contain'}
-      />
-      <h1
-        className={
-          'text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'
-        }
-      >
-        ReactArt
-      </h1>
-      <p className={'text-stone-500'}>A community of artists and art-lovers.</p>
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
     </header>
-  );
+  )
 }
